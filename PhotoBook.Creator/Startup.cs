3a83149c98ca3c12f438bde740ae88643872e70f
Bryan Euton <bryaneuton@gmail.com>
2020-04-28 12:49:51 -0500

    Updated modal dropdown size
    Previously the modal dropdown would be longer than the screen with no way to scroll further down.

--- PhotoBook.Creator/Startup.cs
+++ PhotoBook.Creator/Startup.cs
@@ -128,13 +128,20 @@ namespace PhotoBook.Creator
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+#if DEBUG
+            Logger.Trace("PhotoBook.Creator compiled using DEBUG configuration.");
+#else 
+            Logger.Trace("PhotoBook.Creator compiled using Release configuration.");
+#endif
             if (env.IsDevelopment())
             {
+                Logger.Trace("PhotoBook.Creator utilizing Development mode.");
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
             else
             {
+                Logger.Trace("PhotoBook.Creator utilizing Production mode.");
                 app.UseExceptionHandler(errorApp =>
                 {
                     errorApp.Run(async context =>
